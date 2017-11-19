fe21b4929eacefa576af1fe759bf88be7e3c52ad
David Leonhardt <davidleonhardt@gmail.com>
2017-11-19 03:22:05 -0300

    Added support for scala 2.12

--- src/main/scala/com/github/athieriot/EmbedConnection.scala
+++ src/main/scala/com/github/athieriot/EmbedConnection.scala
@@ -5,12 +5,11 @@ import process.runtime.Network
 import mongo._
 import config._
 import distribution._
-import org.specs2.specification._
-import org.specs2.mutable.SpecificationLike
+import org.specs2.specification.core._
+import org.specs2.mutable.{Specification, SpecificationLike}
 import org.specs2.main.Arguments
 
-trait EmbedConnection extends FragmentsBuilder {
-  self: SpecificationLike =>
+trait EmbedConnection extends Specification { self: SpecificationLike =>
   isolated
 
   override def sequential: Arguments = args(isolated = false, sequential = true)
@@ -18,29 +17,29 @@ trait EmbedConnection extends FragmentsBuilder {
   override def isolated: Arguments = args(isolated = true, sequential = false)
 
   //Override this method to personalize testing port
-  def embedConnectionPort(): Int = { 12345 }
+  def embedConnectionPort: Int = { 12345 }
 
   //Override this method to personalize MongoDB version
-  def embedMongoDBVersion(): Version.Main = { Version.Main.PRODUCTION }
+  def embedMongoDBVersion: Version.Main = { Version.Main.PRODUCTION }
 
   lazy val network = new Net(embedConnectionPort, Network.localhostIsIPv6)
 
-  lazy val mongodConfig = new MongodConfigBuilder()
+  lazy val mongodConfig: IMongodConfig = new MongodConfigBuilder()
     .version(embedMongoDBVersion)
     .net(network)
     .build
 
-  lazy val runtime = MongodStarter.getDefaultInstance
+  lazy val runtime: MongodStarter = MongodStarter.getDefaultInstance
 
-  lazy val mongodExecutable = runtime.prepare(mongodConfig)
+  lazy val mongodExecutable: MongodExecutable = runtime.prepare(mongodConfig)
 
   override def map(fs: => Fragments) = startMongo ^ fs ^ stoptMongo
 
   private def startMongo() = {
-    Example("Start Mongo", { mongodExecutable.start; success })
+    step(mongodExecutable.start)
   }
 
   private def stoptMongo() = {
-    Example("Stop Mongo", { mongodExecutable.stop; success })
+    step(mongodExecutable.stop())
   }
 }
